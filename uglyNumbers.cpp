91955b3bafc56417c9fc3b4398481285b993583c
txf18768115739 <38525118+txf18768115739@users.noreply.github.com>
2018-12-15 02:39:26 +0800

    Update uglyNumbers.cpp

--- uglyNumbers.cpp
+++ uglyNumbers.cpp
@@ -6,26 +6,31 @@
 #include<set>
 using namespace std;
 typedef long long LL;
-
+const int maxn = 20;
 const int coeff[3] = { 2,3,5 };
 
 int main() {
 	priority_queue<LL, vector<LL>, greater<LL> >pq;//优先队列 
 	set<LL> s;//集合 
+	LL a;
 	pq.push(1);//队列插入1
 	s.insert(1);//集合set不重复、按照顺序
 	for (int i = 1;; i++) {
 		LL x = pq.top();//弹出队列的队首元素
+		cout << " the point x " << x << endl;
 		pq.pop();
-		if (i == 1500) {
-			cout << "The 1500'th ugly number is " << x << ".\n";
+		if (i == maxn) {
+			cout << "The maxn ugly number is " << x << ".\n";
 			break;
 		}
 		for (int j = 0; j < 3; j++) {
 			LL x2 = x * coeff[j];
-			if (!s.count(x2)) {
+			cout << " the x2 " << x2<< "the count is"<< s.count(x2) << endl;
+			if (!s.count(x2)) {//如果丑数重复，不push,
 				s.insert(x2);
+				cout << " the x2_1 " << x2 << endl;
 				pq.push(x2);
+				cout << " the x2_2 " << x2 << endl;
 			}
 		}
 	}
