8b61fb2a4657e596315bddaabbc22e6005d5ea4e
txf18768115739 <38525118+txf18768115739@users.noreply.github.com>
2018-12-15 01:56:57 +0800

    输出丑数

---
+++ uglyNumbers.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<set>
+using namespace std;
+typedef long long LL;
+const int coeff[3] = { 2,3,5 };
+
+int main() {
+	priority_queue<LL, vector<LL>, greater<LL> >pq;//优先队列 
+	set<LL> s;//集合 
+	pq.push(1);
+	s.insert(1);
+	for (int i = 1;; i++) {
+		LL x = pq.top();
+		pq.pop();
+		if (i == 1500) {
+			cout << "The 1500'th ugly number is " << x << ".\n";
+			break;
+		}
+		for (int j = 0; j < 3; j++) {
+			LL x2 = x * coeff[j];
+			if (!s.count(x2)) {
+				s.insert(x2);
+				pq.push(x2);
+			}
+		}
+	}
+	system("pause");
+	return 0;
+}
